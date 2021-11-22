32858c9a1dc96c2ad43e10e8538fab31d6b713d0
alexl <alexlam1994@live.com>
2021-11-22 17:33:46 +1100

    added and imported image_controller into _init_.py file

--- side_app/controllers/__init__.py
+++ side_app/controllers/__init__.py
@@ -1,6 +1,7 @@
 from controllers.pharmacy_controller import pharmacies
 from controllers.user_controller import users
+from controllers.image_controller import user_images
 
-registerable_controllers = [pharmacies, users]
+registerable_controllers = [pharmacies, users, user_images]
 
 # Import our new controller and apply that blueprint to our app.
---
+++ side_app/controllers/image_controller.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request, redirect, abort, url_for, current_app
+from pathlib import Path
+from models.users import User
+# S3 from our Flask application we will be using the AWS SDK boto3
+# It is a library that gives us classes and functions to help make our lives easier when we are developing
+import boto3
+
+user_images = Blueprint('user_images', __name__)
+
+@user_images.route("/users/<int:id>/image/", methods=["POST"])
+def update_image(id):
+    user=User.query.filter_by(id=id).first()
+    if "image" in request.files:
+        image = request.files["image"]
+        if Path(image.filename).suffix == ".png":
+            bucket = boto3.resource("s3").Bucket(current_app.config["AWS_S3_BUCKET"])
+            bucket.upload_fileobj(image, f"{user.image_filename}.png")
+        elif Path(image.filename).suffix == ".jpg":
+            bucket = boto3.resource("s3").Bucket(current_app.config["AWS_S3_BUCKET"])
+            bucket.upload_fileobj(image, f"{user.image_filename}.jpg")
+        else:
+            return abort(400, description="Invalid file type")
+        
+        # bucket = boto3.resource("s3").Bucket(current_app.config["AWS_S3_BUCKET"])
+        # bucket.upload_fileobj(image, user.image_filename)
+
+        
+
+        # note that we have removed this line:
+        # image.save(f"static/{course.image_filename}")
+        
+        return redirect(url_for("users.get_user", id=id))
+    return abort(400, description="No image")
